07aa361afbef2c1d3b8368f33d0773ee877b25c2
Mitchell Kennedy <mkennedy@alphatech-computing.com>
2022-02-17 06:20:07 -0500

    Now properly storing tracks and note data in structs and slices.

--- main.go
+++ main.go
@@ -34,12 +34,13 @@ type Tracks struct {
 	track []Track
 }
 
-type score struct {
-	Items []Note
-}
+//type score struct {
+//	Items []Note
+//}
 
 var curScore = Notes{}
-var gScore = Tracks{}
+
+//var gScore = Tracks{}
 var gTracks = Tracks{}
 
 // AddNote adds item to current score array
@@ -49,6 +50,10 @@ func (nt *Notes) AddNote(item Note) {
 
 func (at *Tracks) AddTrack(item Track) {
 	at.track = append(at.track, item)
+}
+
+func (nt *Track) AddNotes(item Notes) {
+	nt.notes = item
 }
 
 func stringInSlice(a string, list []Track) bool {
@@ -61,20 +66,38 @@ func stringInSlice(a string, list []Track) bool {
 	return false
 }
 
+func createTrackIfNotExists(trackNum int16) Track {
+	existingTracks := gTracks.track
+	for _, track := range existingTracks {
+		//println("existing tracks are:")
+		//println(track.number)
+		if track.number == trackNum {
+			//println("brak")
+			return track
+		}
+	}
+	tk := Track{}
+	tk.number = trackNum
+	gTracks.AddTrack(tk)
+	//println("adding track!")
+	//fmt.Println(trackNum)
+	return tk
+}
+
+func getTrackIndex(trackNum int16) int {
+	for index, track := range gTracks.track {
+		if track.number == trackNum {
+			return index
+		}
+	}
+	return 1
+}
+
 func (pr fileReader) noteOn(p *reader.Position, channel, key, vel uint8) {
 	//fmt.Printf("Track: %v Pos: %v NoteOn (ch %v: key %v vel: %v)\n", p.Track, p.AbsoluteTicks, channel, key, vel)
-	tk := Track{}
+	tk := createTrackIfNotExists(p.Track)
 	nt := Note{}
 
-	tk.number = p.Track
-	noteTrackStr := strconv.Itoa(int(p.Track))
-
-	// if track doesn't exist, add it
-	if !stringInSlice(noteTrackStr, gTracks.track) {
-		fmt.Printf("equal!")
-		gTracks.AddTrack(tk)
-	}
-	tk.number = p.Track
 	nt.position.AbsoluteTicks = p.AbsoluteTicks
 	nt.position.Track = p.Track
 	nt.position.DeltaTicks = p.DeltaTicks
@@ -83,19 +106,20 @@ func (pr fileReader) noteOn(p *reader.Position, channel, key, vel uint8) {
 	nt.vel = vel
 	//tk.notes.AddNote()
 	nt.noteOn = true
-	tk.notes.AddNote(nt)
+	tkIndex := getTrackIndex(tk.number)
+	gTracks.track[tkIndex].notes.AddNote(nt)
+	//tk.notes.AddNote(nt)
 }
 
 func (pr fileReader) noteOff(p *reader.Position, channel, key, vel uint8) {
-	tk := Track{}
+	tk := createTrackIfNotExists(p.Track)
 	nt := Note{}
 
-	tk.number = p.Track
 	noteTrackStr := strconv.Itoa(int(p.Track))
 
 	// if track doesn't exist, add it
 	if !stringInSlice(noteTrackStr, gTracks.track) {
-		fmt.Printf("equal!")
+		//fmt.Printf("equal!")
 		gTracks.AddTrack(tk)
 	}
 	tk.number = p.Track
@@ -107,7 +131,8 @@ func (pr fileReader) noteOff(p *reader.Position, channel, key, vel uint8) {
 	nt.vel = vel
 	//tk.notes.AddNote()
 	nt.noteOn = false
-	tk.notes.AddNote(nt)
+	tkIndex := getTrackIndex(tk.number)
+	gTracks.track[tkIndex].notes.AddNote(nt)
 
 }
 
@@ -116,7 +141,7 @@ func (pr fileReader) instrument(p reader.Position, name string) {
 	tk.name = name
 	tk.number = p.Track
 	//fmt.Println(tk.name)
-	gScore.AddTrack(tk)
+	gTracks.AddTrack(tk)
 }
 
 func readMidiFile(midiFilePath string) Tracks {
@@ -125,18 +150,21 @@ func readMidiFile(midiFilePath string) Tracks {
 	rd := reader.New(reader.NoLogger(),
 		//reader.TempoBPM(),
 		//reader.
-		//reader.Instrument(p.instrument),
+		reader.Instrument(p.instrument),
 		reader.NoteOn(p.noteOn),
 		reader.NoteOff(p.noteOff),
 		//reader.EndOfTrack(),
-
 	)
+
 	err := reader.ReadSMFFile(rd, midiFilePath)
 	//fmt.Println(gScore.track)
 
 	if err != nil {
 		fmt.Printf("could not read SMF file %v\n", midiFilePath)
 	}
+	//for _, val := range gTracks.track {
+	//	fmt.Println(val)
+	//}
 	return gTracks
 }
 
@@ -173,8 +201,15 @@ func main() {
 
 	flag.Parse()
 	if *actionPtr == "read" {
-		sc := readMidiFile(*midiFileInPtr)
-		fmt.Printf("%v", sc.track)
+		readMidiFile(*midiFileInPtr)
+		//for _, val := range gTracks.track {
+		//	fmt.Println(val.name)
+		//	for _, nt := range val.notes.msg {
+		//		fmt.Println("note is")
+		//		fmt.Println(nt)
+		//	}
+		//	//
+		//}
 
 	}
 	if *actionPtr == "copy" {
